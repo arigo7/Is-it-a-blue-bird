a839947ec3613b8db87a9d4d6752ada5d5e20791
Ari <ari.go.org.7@gmail.com>
2021-09-21 09:59:59 -0700

    comments and ts upload button to form data

--- src/app/upload-button/upload-button.component.ts
+++ src/app/upload-button/upload-button.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 import { Injectable } from '@angular/core'; 
 import { HttpClient } from '@angular/common/http'; // same as above
 
-// @Injectable() where will this go? https://angular.io/guide/http
+ @Injectable()
 @Component({
   selector: 'app-upload-button',
   templateUrl: './upload-button.component.html',
@@ -19,23 +19,27 @@ export class UploadButtonComponent {
   fileName = '';
 
   constructor(
-    // private http: HttpClient
+    private http: HttpClient
     ) { }
 
   // event will contain the list of files that the user selected on the target.files property
   onFileSelected(event: any) {
+    console.log(event)
     const file:File = event.target.files[0];
 
     //  Prob need to check if type is of image here too, not just file
     if (file) {
       this.fileName = file.name;
-      const formData = new FormData();
-      formData.append("thumbnail", file);
+      // const formData = new FormData();
+      // formData.append("thumbnail", file);
 
       // API VISION? here later
-      // const upload$ = this.http.post("/api/thumbnail-upload", formData);
 
-      // upload$.subscribe()
+    const upload$ = this.http.post("/api/thumbnail-upload", file);
+    // const upload$ = this.http.post("/api/thumbnail-upload", formData);
+
+
+    upload$.subscribe()
     }
   }
 
